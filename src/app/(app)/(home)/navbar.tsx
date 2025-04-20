fc0c318109a80cee64f934e6e2d66bee31127aab
Ashwin Shaji <ashwinshaji154@gmail.com>
2025-04-20 19:10:02 +0530

    Developed the login and the sign up page

--- src/app/(app)/(home)/navbar.tsx
+++ src/app/(app)/(home)/navbar.tsx
@@ -76,13 +76,13 @@ export const Navbar = () => {
           className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg"
           asChild
         >
-          <Link href="/sign-in">Log in</Link>
+          <Link prefetch href="/sign-in">Log in</Link>
         </Button>
         <Button
           className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-black transition-colors text-lg"
           asChild
         >
-          <Link href="/sign-up">Start Selling</Link>
+          <Link prefetch href="/sign-up">Start Selling</Link>
         </Button>
       </div>
       <div className="flex lg:hidden items-center justify-center">
